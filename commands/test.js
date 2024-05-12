eeca25bde5e09b02ea1b4e74971ca92d7f67f595
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2024-05-13 00:23:07 +0100

    Added Test.js (Version 0.0.30)

---
+++ commands/test.js
@@ -0,0 +1,90 @@
+const { sendMessage } = require('../utils');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    name: 'test',
+    description: 'developer testing thing',
+    aliases: ['devtest'],
+    execute(message, args) {
+        if (!isUserWhitelisted(message.author.id)) {
+            if (config.NotAuthorizedMessage) {
+                sendMessage(message, "`SawaSB > You are not authorized to use this command.`")
+            }
+            return;
+        }
+        const infoData = readInfoData();
+        const config = readConfig();
+        const botName = infoData.Name || 'SawaSB';
+
+        sendMessage(message, `.`);
+    },
+};
+
+function readInfoData() {
+    try {
+        const data = fs.readFileSync('./info.log', 'utf8');
+        const lines = data.split('\n');
+        const infoData = {};
+
+        lines.forEach(line => {
+            const [item, value] = line.trim().split(' ');
+            infoData[item] = value;
+        });
+
+        return infoData;
+    } catch (err) {
+        console.error('Error reading info file:', err);
+        return {};
+    }
+}
+
+function readConfig() {
+    try {
+        const configFile = fs.readFileSync('./Config.log', 'utf8');
+        const lines = configFile.split('\n');
+        const cfg = {};
+
+        lines.forEach(line => {
+            if (!line.trim()) {
+                return;
+            }
+
+            if (line.trim().startsWith('//') || line.trim().startsWith('--')) {
+                return;
+            }
+
+            const [key, ...value] = line.trim().split(/\s+/);
+            cfg[key] = value.join(' ');
+        });
+
+        return cfg;
+    } catch (err) {
+        console.error('SawaSB > Error reading Config file:', err);
+        return null;
+    }
+}
+
+function isUserWhitelisted(userId) {
+    try {
+        const whitelistFilePath = path.join(__dirname, '..', 'assets', 'whitelist.log');
+        if (!fs.existsSync(whitelistFilePath)) {
+            console.error('Whitelist file not found.');
+            return false;
+        }
+
+        const whitelistedUsers = fs.readFileSync(whitelistFilePath, 'utf8').split('\n');
+        const cleanedWhitelistedUsers = whitelistedUsers.filter(user => user.trim() !== '');
+
+        for (const user of cleanedWhitelistedUsers) {
+            if (user === userId) {
+                return true;
+            }
+        }
+
+        return false;
+    } catch (error) {
+        console.error('Error checking whitelist:', error);
+        return false;
+    }
+}
